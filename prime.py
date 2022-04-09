a91ec26d0be7c4adffd048ba6ffb73d62437f28c
parksontyc <parkson.tyc.chen@gmail.com>
2022-04-09 11:18:54 +0800

    sec

---
+++ prime.py
@@ -0,0 +1,7 @@
+for i in range(2, 10):
+    for j in range(2, i):
+        if(i % j == 0):
+            print(i, 'is not prime number')
+            break
+    else:
+        print(i, 'is a prime number')
